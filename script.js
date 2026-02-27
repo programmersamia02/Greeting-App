55d2246de8c6179128dbec13dd07c8f0cfac33d4
Samia-Bhuiyan786 <programmersamia02@gmail.com>
2026-02-27 17:11:41 +0600

    Added file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-# Greeting-App
+const greetButton=document.getElementById('greetButton');
+const nameInput=document.getElementById('nameInput');
+const greetingText=document.getElementById('greetingText');
+greetButton.addEventListener('click',function(){
+    const name=nameInput.value.trim();
+    if (name){
+        greetingText.textContent=`Hello, ${name}! Nice to meet you!☺️`;
+
+    } else{
+        greetingText.textContent="Please enter your name!";
+
+    }
+
+});
